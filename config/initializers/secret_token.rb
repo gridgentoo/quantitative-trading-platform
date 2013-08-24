13fc4d092171e21d9ff371cb4960fd5953c03b61
Sheng-Loong Su <sushengloong@gmail.com>
2013-08-24 17:55:48 +0800

    initial commit - blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quantrade::Application.config.secret_key_base = '1c57be23bc05bd8b43b4796e513fa8b4a38909657446db18979e56b46040319841c6f94c55a88ada8b36add641034862932b1d6d6f77885099fcf3703b989b0e'
